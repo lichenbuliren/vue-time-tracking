4c41e22a66821ab9f6fa0513cf6a2bfa1a0dcc31
lichenbuliren <617106268@qq.com>
2016-06-17 18:22:26 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import App from './App';
+import Home from './components/Home.vue';
+
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+// 路由模块和HTTP模块
+Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter();
+
+router.map({
+  '/home': {
+    component: Home,
+  },
+});
+
+router.redirect({
+  '*': '/home',
+});
+
+router.start(App, '#app');
