a39f34952e3c1f5e178f11bb3fcb21ae70a17d3d
lichenbuliren <617106268@qq.com>
2016-06-27 09:57:29 +0800

    添加ESLint注释

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,11 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    // 允许使用 window.alert
     'no-alert': 0,
     // 结尾逗号，分割。
     'comma-dangle': [2, 'never'],
+    // 允许对函数参数重新赋值
     'no-param-reassign': ["error", { "props": false }],
     'import/no-unresolved': 0,
     // allow debugger during development
